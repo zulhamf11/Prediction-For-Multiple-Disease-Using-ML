b9774470db8fba9f28759bff41de6778347b9916
MZulhamF <77193797+zulhamf11@users.noreply.github.com>
2022-07-26 21:43:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from streamlit_option_menu import option_menu
 # loading the saved models
 
 diabetes_model = pickle.load(
-    open('C:/Users/Legion5/ProjectML/saved models/diabetes_model.sav', 'rb'))
+    open('https://github.com/zulhamf11/Prediction-For-Multiple-Disease-Using-ML/blob/master/saved%20models/diabetes_model.sav', 'rb'))
 
 heart_disease_model = pickle.load(
-    open('C:/Users/Legion5/ProjectML/saved models/heart_disease_model.sav', 'rb'))
+    open('https://github.com/zulhamf11/Prediction-For-Multiple-Disease-Using-ML/blob/master/saved%20models/heart_disease_model.sav', 'rb'))
 
 
 # sidebar for navigation
